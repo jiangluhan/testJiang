cf4b3cb5c4268dbee9a7084404c847368766c044
jiangluhan <1352050148@qq.com>
2022-08-19 13:54:26 +0800

    mybatis study 1

---
+++ simple-Mybatis-1/src/main/java/com/tk/simple/mybatis/entity/CountryEntity.java
@@ -0,0 +1,35 @@
+package com.tk.simple.mybatis.entity;
+
+import lombok.Data;
+
+public class CountryEntity {
+    private Integer id;
+
+    private String countryCode;
+
+    private String countryName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+}
