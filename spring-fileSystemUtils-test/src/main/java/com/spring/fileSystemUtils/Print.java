77878dd9b32b4bc1a7aedc45ec1e195cdf2caaee
jiangluhan <1352050148@qq.com>
2022-12-08 15:31:12 +0800

    提交

---
+++ spring-fileSystemUtils-test/src/main/java/com/spring/fileSystemUtils/Print.java
@@ -0,0 +1,7 @@
+package com.spring.fileSystemUtils;
+
+public class Print {
+    public static void print(Object value) {
+        System.out.println(value);
+    }
+}
---
+++ spring-fileSystemUtils-test/src/main/java/com/spring/fileSystemUtils/streamUtils.java
@@ -0,0 +1,11 @@
+package com.spring.fileSystemUtils;
+
+/**
+ * 包含一些文件流的实用方法默认的缓冲区大小为4096bytes
+ * 注意：该工具类的所有方法都不会对流进行关闭处理！
+ */
+public class streamUtils {
+    public static void main(String[] args) {
+
+    }
+}
